c923ec46da8aa84d13983bdab79ab9559d259d54
Ko-stant <kostant.stanton@gmail.com>
2018-02-13 11:51:07 -0500

    Fixes comma missing in scripts
    Added comma to separate values in update and create curl scripts
    
    Tested after pulling origin's model changes.
    
    @mimasofia
    @rcoras

--- scripts/surveys/create.sh
+++ scripts/surveys/create.sh
@@ -10,7 +10,7 @@ curl "${API}${URL_PATH}" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "survey": {
-      "title": "'"${TITLE}"'"
+      "title": "'"${TITLE}"'",
       "question": "'"${QUESTION}"'"
     }
   }'
