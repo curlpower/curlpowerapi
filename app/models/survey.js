7ae06909f01c7bedf38e637675d859a9e8385b89
Ko-stant <kostant.stanton@gmail.com>
2018-02-14 10:59:27 -0500

    Adds subdoc for Survey
    Updates surveySchema model to have a subdoc of  `submissions`.
    
    @mimasofia
    @rcoras

--- app/models/survey.js
+++ app/models/survey.js
@@ -2,6 +2,27 @@
 
 const mongoose = require('mongoose')
 
+const submissionSchema = new mongoose.Schema({
+  answer: {
+    type: Boolean,
+    require: true
+  },
+  _submitter: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Survey',
+    required: true
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    transform: function (doc, ret, options) {
+      const userId = (options.user && options.user._id) || false
+      ret.editable = userId && userId.equals(doc._submitter)
+      return ret
+    }
+  }
+})
+
 const surveySchema = new mongoose.Schema({
   title: {
     type: String,
@@ -11,6 +32,7 @@ const surveySchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  submissions: [submissionSchema],
   _owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
